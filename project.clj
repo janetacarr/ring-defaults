7c60d231d528423ecb86b9247f046f72ac430e1f
Janet Carr <janet.adrianna.carr@gmail.com>
2024-04-07 13:09:23 -0400

    Change to my anti-forgery

--- project.clj
+++ project.clj
@@ -1,14 +1,15 @@
-(defproject ring/ring-defaults "0.4.0"
+(defproject com.janetacarr/ring-defaults "0.4.0"
   :description "Ring middleware that provides sensible defaults"
   :url "https://github.com/ring-clojure/ring-defaults"
   :license {:name "The MIT License"
             :url  "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [ring/ring-core "1.10.0"]
                  [ring/ring-ssl "0.3.0"]
                  [ring/ring-headers "0.3.0"]
-                 [ring/ring-anti-forgery "1.3.0"]
+                 [com.janetacarr/ring-anti-forgery "1.3.0"]
                  [javax.servlet/javax.servlet-api "3.1.0"]]
+  :plugins [[cider/cider-nrepl "0.29.0"]]
   :aliases
   {"test-all" ["with-profile" "default:+1.8:+1.9:+1.10:+1.11" "test"]}
   :profiles
